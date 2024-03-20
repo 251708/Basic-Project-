bdf5dcb2a1c382c87fc21368c787d3ce21478eaf
Sakshi Singh <singhsakshigudiya708@gmail.com>
2024-03-20 12:03:38 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,37 @@
+
+
+
+/*<div class="container">
+  <form id = "form1">
+   <h3>Re form</h3>
+   <input type="email" placeholder="Email" />
+   <input type="email" placeholder="Email" />
+   <input type="email" placeholder="Email" />
+   <div class = "btn_box">
+    <button id = "next1" type="button" >Next1</button>
+   </div>
+
+   <form id = "form2">
+    <h2> Let's Get Started</h2>
+    <h4>Add your details (As per your registered records)</h4>
+    <input type="Name" placeholder="Email" />
+    <input type="Nature of Driving License" placeholder="Email" />
+    <input type="Type of Previous Driving License" placeholder="Email" />
+    <div class = "btn_box">
+     <button id = "next2" type="button" >Next</button>
+    </div>
+
+    <form id = "form3">
+        <h2> Let's Get Started</h2>
+    <h4>Add your details (As per your registered records)</h4>
+        <input type="Place where Driving License was issued" placeholder="Email" />
+        <input type="Issued Year of License" placeholder="Email" />
+        <input type="Type of Previous Driving License" placeholder="Email" />
+        <div class = "btn_box">
+         <button id = "next3" type="button" >Next</button>
+        </div>
+  </form>
+
+
+  </div>
+  */
